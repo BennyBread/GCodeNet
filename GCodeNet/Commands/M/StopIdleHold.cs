69ae967fc142f4ac5c9ed31edd849baba917a348
Peter Lanoie <planoie@gmail.com>
2022-12-01 23:19:46 -0500

    Add Marlin parameters

--- GCodeNet/Commands/M/StopIdleHold.cs
+++ GCodeNet/Commands/M/StopIdleHold.cs
@@ -5,5 +5,21 @@
     {
         [ParameterType("I")]
         public int? ResetFlags { get; set; }
-    }
+
+		[ParameterType("E")]
+		public bool? EDisable { get; set; }
+
+		[ParameterType("S")]
+		public int? InactivityTimeout { get; set; }
+
+		[ParameterType("X")]
+		public bool? XDisable { get; set; }
+
+		[ParameterType("Y")]
+		public bool? YDisable { get; set; }
+
+		[ParameterType("Z")]
+		public bool? ZDisable { get; set; }
+
+	}
 }
